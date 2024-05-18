1b4f06c2468986e3a20067e0d1c8e85912d907df
thour <wargellar@proton.me>
2024-05-18 16:45:54 +0300

    Created User Repository

---
+++ src/main/java/com/thourie/flightreader/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.thourie.flightreader.repositories;
+
+import com.thourie.flightreader.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
