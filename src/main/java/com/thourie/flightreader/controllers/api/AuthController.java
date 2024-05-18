93f127696766485f81b3f24163cea7505d7b2782
thour <wargellar@proton.me>
2024-05-18 18:05:56 +0300

    Add main controller

---
+++ src/main/java/com/thourie/flightreader/controllers/api/AuthController.java
@@ -0,0 +1,27 @@
+package com.thourie.flightreader.controllers.api;
+
+import com.thourie.flightreader.dtos.JwtRequest;
+import com.thourie.flightreader.dtos.RegistrationUserDto;
+import com.thourie.flightreader.servises.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
+        return authService.createAuthToken(authRequest);
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<?> createNewUser(@RequestBody RegistrationUserDto registrationUserDto) {
+        return authService.createNewUser(registrationUserDto);
+    }
+}
