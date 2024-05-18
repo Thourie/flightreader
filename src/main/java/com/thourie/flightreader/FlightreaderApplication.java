9ec8c4ab1f5b33b901be1ba44a7ce988dc66d9a9
thour <wargellar@proton.me>
2024-05-18 15:57:17 +0300

    Initial commit

---
+++ src/main/java/com/thourie/flightreader/FlightreaderApplication.java
@@ -0,0 +1,13 @@
+package com.thourie.flightreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightreaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thourie/flightreader/FlightreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thourie.flightreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
